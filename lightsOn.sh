3605b9a40d3a370e7371985b12c302464c0af46a
Chris L8 <chrisl8@users.noreply.github.com>
2014-10-01 13:12:05 -0500

    Adding an autonomous exploration module called arlobot_explore, and lights!

---
+++ lightsOn.sh
@@ -0,0 +1,3 @@
+~/arlobot/src/arlobot/arlobot_bringup/scripts/drcontrol.py -d A9026EI5 -r 3 -c on
+~/arlobot/src/arlobot/arlobot_bringup/scripts/drcontrol.py -d A9026EI5 -r 4 -c on
+
---
+++ offLights.sh
@@ -0,0 +1,3 @@
+~/arlobot/src/arlobot/arlobot_bringup/scripts/drcontrol.py -d A9026EI5 -r 3 -c off
+~/arlobot/src/arlobot/arlobot_bringup/scripts/drcontrol.py -d A9026EI5 -r 4 -c off
+
