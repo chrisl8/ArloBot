a6405d54341c48d9714472a4b9efa7e0131ee2f8
Chris L8 <chrisl8@users.noreply.github.com>
2014-10-18 15:15:43 -0500

    Fixed bad line breaks

--- auto-explore.sh
+++ auto-explore.sh
@@ -1,3 +1,4 @@
 echo "When you are done, save your map!"
 echo "Please run './save-map.sh mapname' from another terminal when your map is done before closing this!"
 roslaunch metatron_launchers autonomous_explore.launch
+
