59b912a20d552d3e2777a58dc875867373138a4f
Chris L8 <chrisl8@users.noreply.github.com>
2014-10-18 15:58:01 -0500

    Serial write was being called before it was started.

--- src/arlobot/arlobot_bringup/scripts/SerialDataGateway.py
+++ src/arlobot/arlobot_bringup/scripts/SerialDataGateway.py
@@ -11,60 +11,61 @@ import time
 import rospy
 
 def _OnLineReceived(line):
-	print(line)
+    print(line)
 
 
 class SerialDataGateway(object):
-	'''
-	Helper class for receiving lines from a serial port
-	'''
+    '''
+    Helper class for receiving lines from a serial port
+    '''
 
-	def __init__(self, port="/dev/ttyUSB0", baudrate=115200, lineHandler = _OnLineReceived):
-		'''
-		Initializes the receiver class. 
-		port: The serial port to listen to.
-		receivedLineHandler: The function to call when a line was received.
-		'''
-		self._Port = port
-		self._Baudrate = baudrate
-		self.ReceivedLineHandler = lineHandler
-		self._KeepRunning = False
+    def __init__(self, port="/dev/ttyUSB0", baudrate=115200, lineHandler = _OnLineReceived):
+        '''
+        Initializes the receiver class.
+        port: The serial port to listen to.
+        receivedLineHandler: The function to call when a line was received.
+        '''
+        self._Port = port
+        self._Baudrate = baudrate
+        self.ReceivedLineHandler = lineHandler
+        self._KeepRunning = False
 
-	def Start(self):
-		self._Serial = serial.Serial(port = self._Port, baudrate = self._Baudrate, timeout = 1)
+    def Start(self):
+        self._Serial = serial.Serial(port = self._Port, baudrate = self._Baudrate, timeout = 1)
 
-		self._KeepRunning = True
-		self._ReceiverThread = threading.Thread(target=self._Listen)
-		self._ReceiverThread.setDaemon(True)
-		self._ReceiverThread.start()
+        self._KeepRunning = True
+        self._ReceiverThread = threading.Thread(target=self._Listen)
+        self._ReceiverThread.setDaemon(True)
+        self._ReceiverThread.start()
 
-	def Stop(self):
-		rospy.loginfo("Stopping serial gateway")
-		self._KeepRunning = False
-		time.sleep(.1)
-		self._Serial.close()
+    def Stop(self):
+        rospy.loginfo("Stopping serial gateway")
+        self._KeepRunning = False
+        time.sleep(.1)
+        self._Serial.close()
 
-	def _Listen(self):
-		stringIO = StringIO()
-		while self._KeepRunning:
-			data = self._Serial.read()
-			if data == '\r':
-				pass
-			if data == '\n':
-				self.ReceivedLineHandler(stringIO.getvalue())
-				stringIO.close()
-				stringIO = StringIO()
-			else:
-				stringIO.write(data)
-                
-	def Write(self, data):
-		#info = "Writing to serial port: %s" %data
-		#rospy.loginfo(info)
-		self._Serial.write(data)
+    def _Listen(self):
+        stringIO = StringIO()
+        while self._KeepRunning:
+            data = self._Serial.read()
+            if data == '\r':
+                pass
+            if data == '\n':
+                self.ReceivedLineHandler(stringIO.getvalue())
+                stringIO.close()
+                stringIO = StringIO()
+            else:
+                stringIO.write(data)
 
-	if __name__ == '__main__':
-		dataReceiver = SerialDataGateway("/dev/ttyUSB0",  115200)
-		dataReceiver.Start()
+    def Write(self, data):
+        #info = "Writing to serial port: %s" %data
+        #rospy.loginfo(info)
+        if hasattr(SerialDataGateway, '_Serial'): # Don't call if not initialized
+            self._Serial.write(data)
 
-		raw_input("Hit <Enter> to end.")
-		dataReceiver.Stop()
+    if __name__ == '__main__':
+        dataReceiver = SerialDataGateway("/dev/ttyUSB0",  115200)
+        dataReceiver.Start()
+
+        raw_input("Hit <Enter> to end.")
+        dataReceiver.Stop()
