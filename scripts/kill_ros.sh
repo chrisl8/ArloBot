15ea5eedfc07b7fc61c36d6e82e8f8726c068a22
Chris L8 <chrisl8@users.noreply.github.com>
2014-12-18 22:06:41 -0600

    Added startup/kill for ngrok for Twilio SMS message support.

--- scripts/kill_ros.sh
+++ scripts/kill_ros.sh
@@ -1,6 +1,7 @@
 # Attempt to kill ROS if it is already running
 echo "Killing everything, please wait a moment . . ."
 pkill -f metatron_id.launch
+pkill ngrok
 sleep 10
 pkill roslaunch
 sleep 10
--- scripts/start-metatron.sh
+++ scripts/start-metatron.sh
@@ -1,4 +1,6 @@
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 ${SCRIPTDIR}/ros_prep.sh
+# Start web server for Twilio
+ngrok -authtoken E_-s84Q-fW2O1B_kU1Fl -subdomain=52c014b4 -log=stdout 8001 &
 /opt/ros/indigo/bin/roslaunch metatron_id metatron_id.launch --screen
 
