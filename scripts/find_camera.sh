b027e7acdd3b915e79a1e9c0d77b881933f39ac8
Chris L8 <chrisl8@users.noreply.github.com>
2014-12-26 21:02:37 -0600

    Check for invalid command line argument and respond with an error.

--- scripts/find_camera.sh
+++ scripts/find_camera.sh
@@ -1,5 +1,12 @@
 # Find the camera listed on the command line,
 # based on output of 'fswebam'
+if [ $# -eq 0 ]
+then
+echo "You must provide a camera name such as,"
+echo "HP or C615"
+echo "on the command line."
+exit
+fi
 for i in $(ls /dev/video*)
 do
 fswebcam --verbose --device=${i} 2>&1|grep cap.card|grep ${1} > /dev/null
