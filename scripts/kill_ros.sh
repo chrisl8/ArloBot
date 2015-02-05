86e4804e7bc74bf84c43fc9b2a1d9841113c9fa0
Chris L8 <chrisl8@users.noreply.github.com>
2015-02-05 16:33:05 -0600

    Improve kill script to be faster and smarter.

--- scripts/kill_ros.sh
+++ scripts/kill_ros.sh
@@ -1,13 +1,46 @@
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 # Attempt to kill ROS if it is already running
 echo "Killing everything, please wait a moment . . ."
-pkill -f metatron_id.launch
-pkill ngrok
-sleep 10
-pkill roslaunch
-sleep 10
-pkill roscore
-sleep 1
+if (pkill -f metatron_id.launch)
+then
+    while (pgrep -f metatron_id.launch)
+    do
+        echo "Waiting for Metatron to close . . ."
+        sleep 1
+    done
+fi
+if (pkill ngrok)
+then
+    while (pgrep ngrok)
+    do
+        echo "Waiting for ngrok to close . . ."
+        sleep 1
+    done
+fi
+if (pkill -f "arlobot_bringup minimal.launch")
+then
+    while (pgrep -f "arlobot_bringup minimal.launch")
+    do
+        echo "Waiting for Arlobot to close . . ."
+        sleep 1
+    done
+fi
+if (pkill roslaunch)
+then
+    while (pgrep roslaunch)
+    do
+        echo "Waiting for roslaunch to close . . ."
+        sleep 1
+    done
+fi
+if (pkill roscore)
+then
+    while (pgrep roscore)
+    do
+        echo "Waiting for roscore to close . . ."
+        sleep 1
+    done
+fi
 echo "Everything Killed."
 # If you want to be able to reset the ports,
 # you need to add the script resetUSB.sh to the /etc/sudoers file,
