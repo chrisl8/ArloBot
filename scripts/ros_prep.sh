25c43154c28283d0461f2614008e26f3362ebc93
Chris L8 <chrisl8@users.noreply.github.com>
2015-02-05 16:52:20 -0600

    Make startup scripts faster and smarter.

--- scripts/ros_prep.sh
+++ scripts/ros_prep.sh
@@ -2,7 +2,11 @@ SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 # Attempt to kill ROS if it is already running and reset USB Ports
 ${SCRIPTDIR}/kill_ros.sh
 /opt/ros/indigo/bin/roscore &
-sleep 3 # Give roscore time to start before attempting to set parameters
+while ! (rosparam list &> /dev/null)
+do
+    echo "Waiting for roscore to start . . ."
+    sleep 1
+done
 rosparam set /arlobot/port $(${SCRIPTDIR}/find_ActivityBoard.sh)
 rosparam set /joystick/dev $(${SCRIPTDIR}/find_xbox_controller.sh)
 rosparam set /camera1 $(${SCRIPTDIR}/find_camera.sh C615)
