106df5114eb93b7ee4e10b5f91759a2167c44f8a
Chris L8 <chrisl8@users.noreply.github.com>
2015-02-15 20:58:39 -0600

    Add nohup to startup script to further ensure process survive.

--- scripts/start-metatron.sh
+++ scripts/start-metatron.sh
@@ -7,7 +7,7 @@ then
     exit 1
 fi
 # Start web server for Twilio
-ngrok -authtoken E_-s84Q-fW2O1B_kU1Fl -subdomain=52c014b4 -log=stdout 8001 &
-/opt/ros/indigo/bin/roslaunch metatron_id metatron_id.launch --screen &
+nohup ngrok -authtoken E_-s84Q-fW2O1B_kU1Fl -subdomain=52c014b4 -log=stdout 8001 &
+nohup /opt/ros/indigo/bin/roslaunch metatron_id metatron_id.launch --screen &
 echo "Use kill_ros.sh to close."
 
