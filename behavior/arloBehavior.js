4bc9a8fe77a7a8aa109687b79bf17072a49c4a1d
Chris L8 <chrisl8@users.noreply.github.com>
2015-02-15 21:02:18 -0600

    First commit of new node based behavior code.

---
+++ behavior/arloBehavior.js
@@ -0,0 +1,117 @@
+// http://behavior3js.guineashots.com/
+// Setup:
+// npm insatll behavior3js
+// Modifications:
+// package.json:
+//  "main": "libs/behavior3-0.2.0dev.js",
+// behavior3-0.2.0dev.js:
+//this.b3 = this.b3 || {};
+//+b3 = {};
+
+var behavior3js = require('behavior3js');
+
+var arloTree = new b3.BehaviorTree();
+
+var ROSisRunning = b3.Class(b3.Condition);
+ROSisRunning.prototype.name = 'ROSisRunning';
+ROSisRunning.prototype.tick = function(tick) {
+    // Stub tick
+    console.log("ROSisRunning");
+    return b3.FAILURE;
+};
+
+var StartROS = b3.Class(b3.Action);
+StartROS.prototype.name = 'StartROS';
+StartROS.prototype.tick = function(tick) {
+    console.log("Start ROS");
+    return b3.RUNNING;
+};
+
+var UnPlugRobot = b3.Class(b3.Action);
+UnPlugRobot.prototype.name = 'UnPlugRobot';
+UnPlugRobot.prototype.tick = function(tick) {
+    console.log(this.name);
+    return b3.RUNNING;
+};
+
+var LaptopBatteryCharged = b3.Class(b3.Action);
+LaptopBatteryCharged.prototype.name = 'LaptopBatteryCharged';
+LaptopBatteryCharged.prototype.tick = function(tick) {
+    console.log(this.name);
+    return b3.SUCCESS;
+};
+
+var RobotIsUnplugged = b3.Class(b3.Condition);
+RobotIsUnplugged.prototype.name = 'RobotIsUnplugged';
+RobotIsUnplugged.prototype.tick = function(tick) {
+    console.log(this.name);
+    return b3.FAILURE;
+};
+
+var MyAction2 = b3.Class(b3.Action);
+MyAction2.prototype.name = 'MyAction2';
+MyAction2.prototype.tick = function(tick) {
+    console.log(this.name);
+    return b3.FAILURE;
+};
+
+// Build this file with http://behavior3js.guineashots.com/editor/#
+var fs = require('fs');
+// and you can LOAD this data into the editor to start where you left off again
+var arloNodeData = JSON.parse(fs.readFileSync('arloTreeData.json', 'utf8'));
+
+// Despite the Editor creating a beautify JSON behavior tree for us,
+// we still have to list the custom nodes for it by hand.
+//var customNodeNames = {
+    //'ROSisRunning': ROSisRunning,
+    //'StartROS': StartROS,
+    //'UnPlugRobot': UnPlugRobot,
+    //'LaptopBatteryCharged': LaptopBatteryCharged,
+    //'RobotIsUnplugged': RobotIsUnplugged
+//};
+// TODO: Does this work as the better way to do this?
+var customNodeNames= {};
+function parseCustomNodes(element, index, array) {
+    customNodeNames[element.name] = eval(element.name);
+}
+arloNodeData.custom_nodes.forEach(parseCustomNodes);
+//console.log(customNodeNames);
+
+arloTree.load(arloNodeData, customNodeNames);
+
+// This is the Arlobot "object" we will use to track everything,
+// note that there is also a "blackboard" where nodes can track
+// their internal status, even amongst multiple calls
+// to the same node type,
+// but this will keep track of braoder robot settings.
+// NOTE: Do NOT track things here that the nodes
+// should be collecting from real time status!
+// Rather track things we can only know from inside of here,
+// such as last action or speech time, etc.
+var arloBot = {
+    // Currently filled with dummy data
+    flipBit: 1,
+    flipBit2: 1,
+    spokeThisTick: false,
+    spokeLastTick: false
+};
+
+var blackboard = new b3.Blackboard();
+
+console.log('start');
+setInterval(function() {
+    arloTree.tick(arloBot, blackboard);
+    // Note that we can do stuff between ticks if we want to,
+    // although tracking information in the arloBot object,
+    // or node data in the blackboard is preferable.
+    if (arloBot.spokeThisTick) {
+        arloBot.spokeLastTick = true;
+    } else {
+        arloBot.spokeLastTick = false;
+    }
+    arloBot.spokeThisTick = false;
+    //console.log(blackboard);
+    console.log("---");
+}, 1000);
+//tree.tick(arloBot,blackboard);
+console.log('done');
