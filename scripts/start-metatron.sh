ca5b0c86d991595bd6013ad5d0d80b580dbaa69a
Chris L8 <chrisl8@users.noreply.github.com>
2015-02-20 13:47:45 -0600

    Add exit with code.

--- scripts/start-metatron.sh
+++ scripts/start-metatron.sh
@@ -10,4 +10,5 @@ fi
 nohup ngrok -authtoken E_-s84Q-fW2O1B_kU1Fl -subdomain=52c014b4 -log=stdout 8001 &
 nohup /opt/ros/indigo/bin/roslaunch metatron_id metatron_id.launch --screen &
 echo "Use kill_ros.sh to close."
+exit 0
 
