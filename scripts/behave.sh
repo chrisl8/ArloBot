8158285bc8473318f29fefa9740459a2d22225bd
ChrisL8 <ekpyroticfrood@gmail.com>
2015-03-20 13:44:56 -0500

    Rename behavior folder to node to include more functions in it.

--- scripts/behave.sh
+++ scripts/behave.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # The 'courtesy loader' is required for Python to run these directly
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
-cd ${SCRIPTDIR}/../behavior
+cd ${SCRIPTDIR}/../node
 ./arloBehavior.sh
 
