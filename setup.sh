47c81ca7667c23eebbdedd7e227ee834b7208a35
ChrisL8 <ekpyroticfrood@gmail.com>
2015-06-17 09:12:03 -0500

    Update setup script.

--- setup.sh
+++ setup.sh
@@ -5,56 +5,72 @@
 # Run this straight off of github like this:
 # bash <(wget -qO- https://raw.githubusercontent.com/chrisl8/ArloBot/master/setup.sh)
 
-echo ""
-echo "Setting up Robot Operating System for your ArloBot!"
-echo ""
-echo "You will be asked for your password for running commands as root!"
+BLACK='\033[0;30m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+PURPLE='\033[0;35m'
+ORANGE='\033[0;33m' # or brown
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTBLUE='\033[1;34m'
+LIGHTGREEN='\033[1;32m'
+LIGHTCYAN='\033[1;36m'
+LIGHTRED='\033[1;31m'
+LIGHTPURPLE='\033[1;35m'
+YELLOW='\033[1;33m'
+WHITE='\033[1;37m'
+NC='\033[0m' # NoColor
+
+printf "\n${YELLOW}Setting up Robot Operating System for your ArloBot!\n\n"
+printf "${GREEN}You will be asked for your password for running commands as root!\n"
 
 version=`lsb_release -sc`
 
-echo "[Checking the ubuntu version]"
+printf "[Checking the ubuntu version]"
 case $version in
   "saucy" | "trusty")
 ;;
 *)
-echo "[This script will only work on ubuntu saucy(13.10) or trusty(14.04)]"
+printf "[This script will only work on ubuntu saucy(13.10) or trusty(14.04)]"
 exit 0
 esac
 
-echo "[Updating & upgrading all existing Ubuntu packages]"
-echo "silently . . ."
+printf "[Updating & upgrading all existing Ubuntu packages]"
+printf "silently . . ."
 # NOTE: You have to pipe /dev/null INTO apt-get to make it work from wget.
 sudo apt-get update -qq < /dev/null
 sudo apt-get upgrade -qq < /dev/null
 
 # I never use this, but if you are having time issues maybe uncomment this.
-#echo "[Installing chrony and setting the ntpdate]"
+#printf "[Installing chrony and setting the ntpdate]"
 #sudo apt-get install -y chrony
 #sudo ntpdate ntp.ubuntu.com
 
-echo "[Checking for ROS repository]"
+printf "[Checking for ROS repository]"
 if ! [ -e /etc/apt/sources.list.d/ros-latest.list ]
     then
-    echo "[Adding the ROS repository]"
+    printf "[Adding the ROS repository]"
     sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu ${version} main\" > /etc/apt/sources.list.d/ros-latest.list"
-    echo "[Checking the ROS keys]"
+    printf "[Checking the ROS keys]"
     roskey=`apt-key list | grep -i "ROS builder"`
     if [ -z "$roskey" ]
         then
-        echo "[Adding the ROS keys]"
+        printf "[Adding the ROS keys]"
         wget --quiet https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
-        echo "[Update & upgrade the packages again with the new repository]"
-        echo "silently . . ."
+        printf "[Update & upgrade the packages again with the new repository]"
+        printf "silently . . ."
         sudo apt-get update -qq < /dev/null
         sudo apt-get upgrade -qq < /dev/null
     fi
 fi
 
-echo "[Installing ROS!]"
+printf "[Installing ROS!]"
 sudo apt-get install -qy ros-indigo-desktop-full ros-indigo-rqt-* < /dev/null
-echo "[ROS installed.]"
+printf "[ROS installed.]"
 
-echo "[rosdep init and python-rosinstall]"
+printf "[rosdep init and python-rosinstall]"
 if ! [ -e /etc/ros/rosdep/sources.list.d/20-default.list ]
     then
     sudo sh -c "rosdep init"
@@ -63,13 +79,13 @@ rosdep update
 source /opt/ros/indigo/setup.bash
 sudo apt-get install -qy python-rosinstall < /dev/null
 
-echo "[Installing ROS Packages for Arlo]"
+printf "[Installing ROS Packages for Arlo]"
 sudo apt-get install -qy ros-indigo-turtlebot ros-indigo-turtlebot-apps ros-indigo-turtlebot-interactions ros-indigo-turtlebot-simulator ros-indigo-kobuki-ftdi < /dev/null
 source /opt/ros/indigo/setup.bash
 
 if ! [ -d ~/catkin_ws/src ]
     then
-    echo "[Making the catkin workspace and testing the catkin_make]"
+    printf "[Making the catkin workspace and testing the catkin_make]"
     mkdir -p ~/catkin_ws/src
     cd ~/catkin_ws/src
     catkin_init_workspace
@@ -81,7 +97,7 @@ source ~/catkin_ws/devel/setup.bash
 rospack profile
 cd ~/catkin_ws/src
 
-echo "[Cloning or Updating git repositories]"
+printf "[Cloning or Updating git repositories]"
 if ! [ -d ~/catkin_ws/src/hector_slam ]
     then
     git clone https://github.com/chrisl8/hector_slam.git
@@ -147,7 +163,7 @@ catkin_make
 source ~/catkin_ws/devel/setup.bash
 rospack profile
 
-echo "[Setting the ROS evironment]"
+printf "[Setting the ROS evironment]"
 if ! (grep ROS_HOSTNAME ~/.bashrc>/dev/null)
     then
     sh -c "echo \"export ROS_HOSTNAME=`uname -n`.local\" >> ~/.bashrc"
@@ -168,13 +184,13 @@ fi
 
 if ! (id|grep dialout>/dev/null)
     then
-    echo "Adding your user to the dialout group,"
-    echo "You may be asked for your password."
+    printf "Adding your user to the dialout group,"
+    printf "You may be asked for your password."
     sudo adduser ${USER} dialout
-    echo "You may have to reboot before you can use the Propeller Board."
+    printf "You may have to reboot before you can use the Propeller Board."
 fi
 
-echo "Installing additional required Ubuntu packages for Arlobot"
+printf "Installing additional required Ubuntu packages for Arlobot"
 
 # For 8-CH USB Relay board:
 # Reference: https://code.google.com/p/drcontrol/wiki/Install_RaspberryPi">https://code.google.com/p/drcontrol/wiki/Install_RaspberryPi
@@ -191,10 +207,10 @@ sudo pip install pylibftdi
 
 if ! [ -f /etc/udev/rules.d/99-libftdi.rules ]
     then
-    echo "Adding required sudo rule to reset USB ports."
-    echo "You may be asked for your password."
+    printf "Adding required sudo rule to reset USB ports."
+    printf "You may be asked for your password."
     sudo ~/catkin_ws/src/ArloBot/addRuleForUSBRelayBoard.sh
-    echo "You may have to reboot before the USB Relay board will function!"
+    printf "You may have to reboot before the USB Relay board will function!"
 fi
 
 # We will use ~/.arlobot to store "private" data
@@ -212,26 +228,26 @@ if [ -e ${ARLOHOME}/arlobot.yaml ]
     then
     if ! (diff ${HOME}/catkin_ws/src/ArloBot/src/arlobot/arlobot_bringup/param/arlobot.yaml ${ARLOHOME}/arlobot.yaml)
         then
-        echo ""
-        echo "The arlobot.yaml file in the repository is different from the one"
-        echo "in your local settings."
-        echo "This is expected, but just in case, please look over the differences,"
-        echo "and see if you need to copy in any new settings, or overwrite the file completely:"
+        printf ""
+        printf "The arlobot.yaml file in the repository is different from the one"
+        printf "in your local settings."
+        printf "This is expected, but just in case, please look over the differences,"
+        printf "and see if you need to copy in any new settings, or overwrite the file completely:"
         diff ${HOME}/catkin_ws/src/ArloBot/src/arlobot/arlobot_bringup/param/arlobot.yaml ${ARLOHOME}/arlobot.yaml
         cp -i ${HOME}/catkin_ws/src/ArloBot/src/arlobot/arlobot_bringup/param/arlobot.yaml ${ARLOHOME}/
-        echo ""
+        printf ""
     fi
 else
-    echo ""
+    printf ""
     cp ${HOME}/catkin_ws/src/ArloBot/src/arlobot/arlobot_bringup/param/arlobot.yaml ${ARLOHOME}/
-    echo "A brand new ~/.arlobot/arlobot.yaml file has been created,"
-    echo "please edit this file to customize according to your robot!"
-    echo ""
+    printf "A brand new ~/.arlobot/arlobot.yaml file has been created,"
+    printf "please edit this file to customize according to your robot!"
+    printf ""
 fi
 
-echo "All done! Reboot and start testing!"
-echo "Look at README.md for testing ideas."
-echo ""
-echo "See here for your next step:"
-echo "http://ekpyroticfrood.net/?p=165"
-echo ""
+printf "All done! Reboot and start testing!"
+printf "Look at README.md for testing ideas."
+printf ""
+printf "See here for your next step:"
+printf "http://ekpyroticfrood.net/?p=165"
+printf ""
