442bb48128837a6e4c8fd4611e69553baae6184a
ChrisL8 <ekpyroticfrood@gmail.com>
2015-06-18 21:17:09 -0500

    Make startup scripts more confirgurable.

--- scripts/check_hardware.sh
+++ scripts/check_hardware.sh
@@ -1,33 +1,50 @@
-# Check that all required hardware is present
+# This script checks that all required hardware is present
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+
 # USB Relay Controller
-${SCRIPTDIR}/drcontrol.py -l|grep USB &> /dev/null
-if [ $? -gt 0 ]
+if [ $(jq '.useUSBrelay' ${HOME}/.arlobot/personalDataForBehavior.json) == true
     then
-    echo "USB Relay Controller missing!"
-    exit 1
+    ${SCRIPTDIR}/drcontrol.py -l|grep USB &> /dev/null
+    if [ $? -gt 0 ]
+        then
+        echo "USB Relay Controller missing!"
+        exit 1
+    fi
 fi
+
 # Camera 0
-ls /dev/video0 &> /dev/null
-if [ $? -gt 0 ]
+if [ $(jq '.camera0' ${HOME}/.arlobot/personalDataForBehavior.json) == true
     then
-    echo "Camera 0 Missing!"
-    exit 1
+    ls /dev/video0 &> /dev/null
+    if [ $? -gt 0 ]
+        then
+        echo "Camera 0 Missing!"
+        exit 1
+    fi
 fi
+
 # Camera 1
-ls /dev/video1 &> /dev/null
-if [ $? -gt 0 ]
+if [ $(jq '.camera1' ${HOME}/.arlobot/personalDataForBehavior.json) == true
     then
-    echo "Camera 1 Mising!"
-    exit 1
+    ls /dev/video1 &> /dev/null
+    if [ $? -gt 0 ]
+        then
+        echo "Camera 1 Mising!"
+        exit 1
+    fi
 fi
+
 # Activity Board
-${SCRIPTDIR}/find_ActivityBoard.sh |grep USB &> /dev/null
-if [ $? -gt 0 ]
+if [ $(jq '.hasActivityBoard' ${HOME}/.arlobot/personalDataForBehavior.json) == true
     then
-    echo "Activity Board missing!"
-    exit 1
+    ${SCRIPTDIR}/find_ActivityBoard.sh |grep USB &> /dev/null
+    if [ $? -gt 0 ]
+        then
+        echo "Activity Board missing!"
+        exit 1
+    fi
 fi
+
 # Quick Start Board
 # While it should exist, it does not have to be plugged into the computer for Arlo to operate
 #${SCRIPTDIR}/find_QuickStart.sh |grep USB &> /dev/null
@@ -36,6 +53,7 @@ fi
 #echo "Quick Start Board missing!"
 #exit 1
 #fi
+
 # XV-11
 if [ $(jq '.use_xv11' ${HOME}/.arlobot/personalDataForBehavior.json) == true
     then
