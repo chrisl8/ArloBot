d79416edd63fb5f3b38d39462c3def38c9924eeb
ChrisL8 <ekpyroticfrood@gmail.com>
2015-10-02 15:52:10 -0500

    Fix Log Streamer startup script.

--- scripts/log-watcher.sh
+++ scripts/log-watcher.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Grab and save the path to this script
 # http://stackoverflow.com/a/246128
 SOURCE="${BASH_SOURCE[0]}"
@@ -23,10 +24,12 @@ echo "exports.config = {" > ${confFile}
 echo "  nodeName: \"application_server\"," >> ${confFile}
 echo "  logStreams: {" >> ${confFile}
 firstLine=true
-for i in $(ls ${HOME}/.ros/log)
+#for i in $(ls ${HOME}/.ros/log)
+for i in $(ls -d ${HOME}/.ros/log/*/)
 do
     logFolder=${i}
-    for j in $(ls ${HOME}/.ros/log/${logFolder})
+    #for j in $(ls ${HOME}/.ros/log/${logFolder})
+    for j in $(ls ${logFolder})
     do
         logName=$(echo ${j}|sed 's/.log//')
         if [ "${firstLine}" = false ]
@@ -35,9 +38,24 @@ do
         fi
         firstLine=false
         echo "  \"${logName}\": [" >> ${confFile}
-        echo "    \"${HOME}/.ros/log/${logFolder}/${j}\"" >> ${confFile}
+        echo "    \"${logFolder}${j}\"" >> ${confFile}
     done
 done
+
+# Grab the log files in the ~/.ros/log folder itself
+logFolder=${HOME}/.ros/log
+for j in $(find ${logFolder} -maxdepth 1 -type f|sed 's#.*/##')
+do
+    logName=$(echo ${j}|sed 's/.log//')
+    if [ "${firstLine}" = false ]
+    then
+        echo "  ]," >> ${confFile}
+    fi
+    firstLine=false
+    echo "  \"${logName}\": [" >> ${confFile}
+    echo "    \"${logFolder}/${j}\"" >> ${confFile}
+done
+
 if [ -f /tmp/robotNodeScript.log ]
     then
 echo "  ]," >> ${confFile}
