28e964e12a04b83ee21fa4a1c44f8a09b9e5f94e
Chris L8 <chrisl8@users.noreply.github.com>
2016-01-29 14:49:13 -0600

    Add description.

--- scripts/model-robot.sh
+++ scripts/model-robot.sh
@@ -1,3 +1,9 @@
+# This will launch RVIZ and the ROS Robot Model
+# on your local system so that you can view the robot
+# model in RVIZ for tweaking the look and part location
+# without having to run the entire ROS robot stack.
+# I use this to work on the model from my desktop,
+# instead of having to do it from the laptop on the robot.
 export ROS_MASTER_URI=http://localhost:11311
 roslaunch arlobot_bringup model_robot.launch
 
