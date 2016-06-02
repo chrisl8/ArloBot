591ba08cb16a09ddcab764b7560ad41ffd6cf610
Chris L8 <chrisl8@users.noreply.github.com>
2016-06-02 08:52:51 -0500

    Update node to latest version.

--- scripts/setNodeVersion.sh
+++ scripts/setNodeVersion.sh
@@ -2,6 +2,5 @@
 # All this does is set what node version all of my stuff uses.
 # Since the merge and update to 4.0, I have found a need to stick to a specific relaese to keep
 # things stable
-#export node_version=5.10.1
-export node_version=6.1.0
+export node_version=6.2.0
 
