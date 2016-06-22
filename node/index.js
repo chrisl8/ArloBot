2e58e6b57e0c1cd28c2daab92acf92f37c2f32a0
Chris L8 <chrisl8@users.noreply.github.com>
2016-06-22 15:04:21 -0500

    Use more accurate ROS startup check string.

--- node/index.js
+++ node/index.js
@@ -826,8 +826,8 @@ arloTree.load(arloNodeData, customNodeNames);
 robotModel.ROSprocess = new LaunchScript({
     name: 'ROS',
     scriptName: '../scripts/start-robot.sh',
-    successString: 'process[arlobot-4]: started with pid' // NOTE: The number (4) will change if the number ROS launch processes changes! i.e. when we removed something from the arlobot_launchers/launch/robot.launch file, it went from 6 to 5!
-    // TODO: This could be set to "true" by the ROSlibJS connection instead?!
+    successString: '] Propellerbot_node has started.'
+    // TODO: Could this be set to "true" by the ROSlibJS connection instead??
 });
 
 /* GotoWaypoint Process output:
