7d332aa3e08a7c346f24267f12329ae22548baca
Chris L8 <chrisl8@users.noreply.github.com>
2016-12-17 15:29:47 -0600

    Update serial test info for DHB-10 motor controller.

--- scripts/direct2PropSerialTest.sh
+++ scripts/direct2PropSerialTest.sh
@@ -28,6 +28,9 @@ echo "i     0"
 echo "i     0"
 echo "etc."
 echo "Paste this line into the terminal to get it to start sending sensor data:"
+echo "For HB-25:"
 echo "d,0.403000,0.006760,0,0,0,0,0,0.0,0.0,0.0"
+echo "For DHB-10:"
+echo "d,0.403000,0.00338,0,0,0,0,0,0.0,0.0,0.0"
 miniterm.py $(${SCRIPTDIR}/find_ActivityBoard.sh) 115200
 
