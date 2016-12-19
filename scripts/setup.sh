a4a72c752c61aa435332f97c27da6f03467a66e7
Chris L8 <chrisl8@users.noreply.github.com>
2016-12-19 15:43:15 -0600

    Update nvm version in setup.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -40,7 +40,7 @@ if [ -e  ${HOME}/.nvm/nvm.sh ]
     nvm deactivate
 fi
 
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 export NVM_DIR="${HOME}/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
