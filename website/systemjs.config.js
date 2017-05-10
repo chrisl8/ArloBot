788ccff3858e40b18a1c1d529c2c92e0fe8db433
Chris L8 <chrisl8@users.noreply.github.com>
2017-05-10 13:52:37 -0500

    Fix ng2-bootstrap file name to comply with name in the latest npm package.

--- website/systemjs.config.js
+++ website/systemjs.config.js
@@ -25,7 +25,7 @@
             'rxjs': 'npm:rxjs',
             'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
             'moment': 'node_modules/moment/moment.js',
-            'ng2-bootstrap/ng2-bootstrap': 'node_modules/ng2-bootstrap/bundles/ng2-bootstrap.umd.js',
+            'ng2-bootstrap/ng2-bootstrap': 'node_modules/ng2-bootstrap/bundles/ngx-bootstrap.umd.js',
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
