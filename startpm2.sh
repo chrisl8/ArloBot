1300cfc6e90a844eb107dea4303d92c6515dbfbb
Chris L8 <chrisl8@users.noreply.github.com>
2017-06-28 08:47:16 -0500

    PM2 node watching service will now shut off relays when it first starts. This way if the robot computer is rebooted, it will shut off the power first thing.

--- startpm2.sh
+++ startpm2.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 export NVM_DIR="${HOME}/.nvm"
 [ -s "$NVM_DIR/nvm.sh"  ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# USB Relay Controller
+if [ $(jq '.useUSBrelay' ${HOME}/.arlobot/personalDataForBehavior.json) == true ]
+    then
+    echo "Turning off all relays"
+    ${HOME}/catkin_ws/src/ArloBot/scripts/switch_relay_name.sh all off
+fi
+
+# Master Power Relay
+if [ $(jq '.useMasterPowerRelay' ${HOME}/.arlobot/personalDataForBehavior.json) == true  ]
+then
+    echo "Turning off Arlo Power."
+    ${HOME}/catkin_ws/src/ArloBot/scripts/switch_master_relay.sh off
+    # It takes a while before it works after boot up.
+    while [ $? -gt 0 ]
+    do
+        sleep 1
+        ${HOME}/catkin_ws/src/ArloBot/scripts/switch_master_relay.sh off
+    done
+    # The first one always results in some odd output and a failure, so do it one more time.
+    sleep 1
+    ${HOME}/catkin_ws/src/ArloBot/scripts/switch_master_relay.sh off
+fi
+
 cd ${HOME}/catkin_ws/src/ArloBot/node/
 if [ $(jq '.useMyCroft' ${HOME}/.arlobot/personalDataForBehavior.json) == true ]
     then
