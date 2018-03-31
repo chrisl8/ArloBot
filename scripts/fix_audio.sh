30b0a3794c81e7be1a06a236b6459986e1269f6f
Chris L8 <chrisl8@users.noreply.github.com>
2018-03-31 16:42:51 -0500

    Add script to fix audio when it is broken.

---
+++ scripts/fix_audio.sh
@@ -0,0 +1,4 @@
+# https://help.ubuntu.com/community/SoundTroubleshootingProcedure
+# https://wiki.archlinux.org/index.php/PulseAudio/Examples#Set_the_default_output_source
+killall pulseaudio; rm -r ~/.config/pulse/*
+
