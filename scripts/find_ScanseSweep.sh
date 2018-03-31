7e4f4529613cb1700452808c5570ad04822bb9b0
Chris L8 <chrisl8@users.noreply.github.com>
2018-03-31 16:42:16 -0500

    Add required finder code for Scanse Sweep.

---
+++ scripts/find_ScanseSweep.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Grab and save the path to this script
+# http://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+# echo ${SCRIPTDIR} # For debugging
+
+node ${SCRIPTDIR}/../node/UsbDevice.js "FT230X_Basic_UART" ID_MODEL
