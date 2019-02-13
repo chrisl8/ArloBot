7b19b357855610814857b78533d0c8b7c42874b9
Chris L8 <chrisl8@users.noreply.github.com>
2019-02-12 18:09:17 -0600

    Minor updates to make the linter gods happy.

--- scripts/view-navigation.sh
+++ scripts/view-navigation.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-if [ $(jq '.cloudServer.exists' ${HOME}/.arlobot/personalDataForBehavior.json) == true ]
-    then
+
+if [[ $(jq '.cloudServer.exists' ${HOME}/.arlobot/personalDataForBehavior.json) == true ]]; then
     node ${HOME}/catkin_ws/src/ArloBot/node/addRobotIpToEtcHosts.js > /dev/null
-    if [ $? -gt 0 ]
-        then
+    if [[ $? -gt 0 ]]; then
         echo "You will be asked for your password in order to have root access to edit /etc/hosts"
         echo "to add the host/ip for your robot."
         export NVM_DIR="$HOME/.nvm"
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+        [[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+        [[ -s "$NVM_DIR/bash_completion" ]] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
         sudo ${HOME}/.nvm/versions/node/$(nvm version)/bin/node ${HOME}/catkin_ws/src/ArloBot/node/addRobotIpToEtcHosts.js
     fi
 fi
