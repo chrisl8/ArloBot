02636df90a79c53db931271bfb6ef67fd933f496
Chris L8 <ekpyroticfrood@gmail.com>
2019-07-30 14:00:52 -0500

    Update script to ES6.

--- scripts/listMaps.sh
+++ scripts/listMaps.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 // This is an example of how to call a node module from the command line,
-var getMapList = require('../node/getMapList.js');
-var mapDir = process.env.HOME + '/.arlobot/rosmaps/';
-var formatOutput = function(err, data) {
+const getMapList = require('../node/getMapList.js');
+const mapDir = process.env.HOME + '/.arlobot/rosmaps/';
+const formatOutput = function(err, data) {
     data.forEach(function(value) {
         console.log(value.replace('.yaml', ''));
     });
