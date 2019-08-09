be11b789ceaa9087c608c1b15ef6c22776666c07
Chris L8 <ekpyroticfrood@gmail.com>
2019-08-09 11:48:40 -0500

    Add convenience script to ignore all sensors for testing.

---
+++ scripts/ignoreAllSensors.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+rosparam set /arlobot/ignoreCliffSensors true
+rosparam set /arlobot/ignoreFloorSensors true
+rosparam set /arlobot/ignoreIRSensors true
+rosparam set /arlobot/ignoreProximity true
+rosparam set /arlobot/monitorACconnection false
