5388a1891a7617ef47cf582614ab93eba8c3215a
Chris L8 <ChristenLofland@gmail.com>
2019-11-06 17:52:32 -0600

    More convenience scripts.

---
+++ scripts/rainbowCycleLCD.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Grab and save the path to this script
+# http://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+# echo "${SCRIPTDIR}" # For debugging
+
+while read -r i; do node "${SCRIPTDIR}/../node/LCD.js" color "${i}"; done <"${SCRIPTDIR}/colorList.txt"
---
+++ scripts/staticTransformExample.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# This is meant to remind/help us know how to create static transforms from the command line
+# The online documentation is here, but it is lacking:
+# http://wiki.ros.org/tf#static_transform_publisher
+
+# First, you have to put "rosrun tf " in front of it.
+
+# Second, there are two forms, and they are entirely devined from the number of parameters.
+# Either:
+#static_transform_publisher x y z yaw pitch roll frame_id child_frame_id period_in_ms
+
+# or
+
+#static_transform_publisher x y z qx qy qz qw frame_id child_frame_id  period_in_ms
+
+# Note the use of "yaw pitch roll" or "qx qy qz qw"
+
+# rpy in the URD files is for yaw, ptch, roll
+
+# Examples:
+#rosrun tf static_transform_publisher 0.0 0.0 0.0 0.0 0.0 0.0 1.0 base_link map 1000
+#rosrun tf static_transform_publisher cam_px asus_xtion_pro_cam_py cam_pz cam_or cam_op cam_oy base_link camera_rgb_frame 1000
+#rosrun tf static_transform_publisher 0.1085 0.0205 0.17 0.0 0.0 0.0 base_link camera_rgb_frame 1000
+rosrun tf static_transform_publisher 0.1285 0.01 0.17 0.0 0.0 0.0 1.0 base_link camera_rgb_frame 1000
