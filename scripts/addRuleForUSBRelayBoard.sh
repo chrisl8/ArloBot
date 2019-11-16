1286b74363021fbdd19694cf0b230f9d7a2e0067
Chris L8 <ChristenLofland@gmail.com>
2019-11-16 10:46:40 -0600

    Use wildcard in udev rules to cover all cases.

--- scripts/addRuleForUSBRelayBoard.sh
+++ scripts/addRuleForUSBRelayBoard.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", GROUP="dialout", MODE="0660"' >/etc/udev/rules.d/99-libftdi.rules
-echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6014", GROUP="dialout", MODE="0660"' >>/etc/udev/rules.d/99-libftdi.rules
+echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="*", GROUP="dialout", MODE="0660"' >/etc/udev/rules.d/99-libftdi.rules
