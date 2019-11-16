491ddaaa13df558470e622920520ab47b6f459d7
Chris L8 <ChristenLofland@gmail.com>
2019-11-16 11:50:25 -0600

    Update Camera Finder script to deal with new Kernel multi device system.

--- scripts/find_camera.sh
+++ scripts/find_camera.sh
@@ -3,15 +3,21 @@
 # based on output of 'fswebam'
 if [[ $# -eq 0 ]]; then
   echo "You must provide a camera name such as,"
-  echo "HP or C615"
+  echo "HP or C615 or 'Integrated Camera'"
   echo "on the command line."
   exit
 fi
 FOUND=1
 for i in /dev/video*; do
   if fswebcam --verbose --device="${i}" 2>&1 | grep cap.card | grep "${1}" >/dev/null; then
-    echo "${i}"
-    FOUND=0
+    # As of recent kernel revisions, Linux adds TWO /dev/video* interfaces for each camera!
+    # One of them is just for extra metadata
+    # https://unix.stackexchange.com/questions/512759/multiple-dev-video-for-one-physical-device
+    # So we have to find the right one.
+    if fswebcam --verbose --device="${i}" 2>&1 | grep -e 'YUYV' -e 'MJPG' >/dev/null; then
+      echo "${i}"
+      FOUND=0
+    fi
   fi
 done
 exit ${FOUND}
