738d67144d77c650921c3c70ea9b786e3f0bdf9f
Chris L8 <ChristenLofland@gmail.com>
2019-11-16 17:10:32 -0600

    Add a timeout to to the startpm2 attempt to shut down the master relay so that it does not loop for eternity.

--- startpm2.sh
+++ startpm2.sh
@@ -9,14 +9,19 @@ if [[ "$(jq '.useUSBrelay' "${HOME}"/.arlobot/personalDataForBehavior.json)" == 
   "${HOME}"/catkin_ws/src/ArloBot/scripts/switch_relay_name.sh all off
 fi
 
+MAX_RETRY_COUNT=10
+RETRY_COUNT=0
+
 # Master Power Relay
 if [[ "$(jq '.useMasterPowerRelay' "${HOME}"/.arlobot/personalDataForBehavior.json)" == true ]]; then
   echo "Turning off Arlo Power."
 
   # It takes a while before it works after boot up.
-  while ! "${HOME}"/catkin_ws/src/ArloBot/scripts/switch_master_relay.sh off; do
+  while ! "${HOME}"/catkin_ws/src/ArloBot/scripts/switch_master_relay.sh off && [[ ${RETRY_COUNT} -lt ${MAX_RETRY_COUNT} ]]; do
+    echo ${RETRY_COUNT}
     sleep 1
     "${HOME}"/catkin_ws/src/ArloBot/scripts/switch_master_relay.sh off
+    RETRY_COUNT=$((RETRY_COUNT + 1))
   done
   # The first one always results in some odd output and a failure, so do it one more time.
   sleep 1
