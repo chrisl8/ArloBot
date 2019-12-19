513f7ae2815203730829eef02842a3210519dc6c
Christen Lofland <ChristenLofland@gmail.com>
2019-12-19 09:53:53 -0600

    Add x11 docker shortcut to run ROS reconfigure.

---
+++ scripts/docker-run-reconfigure.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Grab and save the path to this script
+# http://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [[ -L "$SOURCE" ]]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ ${SOURCE} != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+#echo "${SCRIPT_DIR}" # For debugging
+
+echo "NOTE: Sometimes being on a different network than normal,"
+echo "like WiFi vs. LAN can break this, or parts of it like 2D Nav Goals."
+"${SCRIPT_DIR}/../docker-rviz/call-via-x11docker.sh" "/runReconfigure.sh"
