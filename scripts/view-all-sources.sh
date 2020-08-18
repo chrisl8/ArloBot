7af64243a638dd7f562766a6294057812d84b5e1
Christen Lofland <ChristenLofland@gmail.com>
2020-08-18 11:24:46 -0500

    Add RVIZ view for testing all lidar inputs.

---
+++ scripts/view-all-sources.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if (command -v jq >/dev/null) && [[ $(jq '.cloudServer.exists' "${HOME}/.arlobot/personalDataForBehavior.json") == true ]]; then
+  if ! node "${HOME}/catkin_ws/src/ArloBot/node/addRobotIpToEtcHosts.js" >/dev/null; then
+    echo "You will be asked for your password in order to have root access to edit /etc/hosts"
+    echo "to add the host/ip for your robot."
+    export NVM_DIR="$HOME/.nvm"
+    # shellcheck source=/home/chrisl8/.nvm/nvm.sh
+    [[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    sudo "${HOME}/.nvm/versions/node/$(nvm version)/bin/node" "${HOME}/catkin_ws/src/ArloBot/node/addRobotIpToEtcHosts.js"
+  fi
+fi
+echo "If you do not have a map loaded,"
+echo "set Global Options->Fixed Frame to"
+echo "'odom' in order to make this work."
+roslaunch arlobot_ros view_all_sources.launch --screen
