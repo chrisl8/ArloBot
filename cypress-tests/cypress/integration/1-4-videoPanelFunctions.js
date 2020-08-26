f0801bb3875fc35ea43d3dd85b8c48f4353e9605
Christen Lofland <ChristenLofland@gmail.com>
2020-08-25 22:34:07 -0500

    Update video panel cypress check for my current setup.

--- cypress-tests/cypress/integration/1-4-videoPanelFunctions.js
+++ cypress-tests/cypress/integration/1-4-videoPanelFunctions.js
@@ -4,12 +4,12 @@ import {
   setSoundToQuiet,
   setIdleToTimeout,
   openPanelIfClosed,
-  closePanelIfOpen
+  closePanelIfOpen,
 } from "../support/reusableTestsAndSetupTasks";
 
 import {
   videoPanelShouldBeOpen,
-  startupShutdownPanelShouldBeOpen
+  startupShutdownPanelShouldBeOpen,
 } from "../support/panelTestsWithRosOff";
 
 describe("Behavior Panel Functions", () => {
@@ -31,9 +31,7 @@ describe("Behavior Panel Functions", () => {
   it("turn on camera", () => {
     cy.contains("Video - Camera Off").should("be.visible");
 
-    cy.get("#fiveVoltRelayButton")
-      .contains("span", "Off")
-      .should("be.visible");
+    cy.get("#fiveVoltRelayButton").contains("span", "Off").should("be.visible");
     cy.get("#fiveVoltRelayButton").should("not.have.class", "btn-success");
 
     cy.get("#masterRelayStatusButton")
@@ -41,46 +39,22 @@ describe("Behavior Panel Functions", () => {
       .should("be.visible");
     cy.get("#masterRelayStatusButton").should("not.have.class", "btn-success");
 
-    cy.get("#cameraStatusButton")
-      .contains("span", "Off")
-      .should("be.visible");
+    cy.get("#cameraStatusButton").contains("span", "Off").should("be.visible");
     cy.get("#cameraStatusButton").should("not.have.class", "btn-success");
 
-    cy.get("#cameraButton")
-      .contains("span", "Off")
-      .should("be.visible");
+    cy.get("#cameraButton").contains("span", "Off").should("be.visible");
     cy.get("#videoFeed").should("be.visible");
     cy.get("#cameraButton").should("not.have.class", "btn-success");
 
     cy.get("#cameraButton").click();
 
-    cy.contains("Camera will be up soon . . .").should("be.visible");
-    cy.contains("Relay 7 on").should("be.visible");
-
-    cy.get("#fiveVoltRelayButton")
-      .contains("span", "On")
-      .should("be.visible");
-    cy.get("#fiveVoltRelayButton").should("have.class", "btn-success");
-
-    cy.contains("Switching Master Relay on.").should("be.visible");
-    cy.contains("Master Relay on").should("be.visible");
-
-    cy.get("#masterRelayStatusButton")
-      .contains("span", "On", { timeout: 15000 })
-      .should("be.visible");
-    cy.get("#masterRelayStatusButton").should("have.class", "btn-success");
-
     cy.contains("Finding Camera C615", { timeout: 10000 }).should("be.visible");
     cy.contains("Starting Camera").should("be.visible");
 
-    cy.get("#cameraStatusButton")
-      .contains("span", "On")
-      .should("be.visible");
+    cy.get("#cameraStatusButton").contains("span", "On").should("be.visible");
     cy.get("#cameraStatusButton").should("have.class", "btn-success");
 
-    cy.get("#cameraButton")
-      .contains("span", "On")
-      .should("be.visible");
+    cy.get("#cameraButton").contains("span", "On").should("be.visible");
     cy.get("#cameraButton").should("have.class", "btn-success");
     cy.get("#videoFeed").should("be.visible");
     cy.contains("Camera ON").should("be.visible");
@@ -98,32 +72,15 @@ describe("Behavior Panel Functions", () => {
 
     cy.contains("Camera ended normally.").should("be.visible");
 
-    cy.get("#cameraStatusButton")
-      .contains("span", "Off")
-      .should("be.visible");
+    cy.get("#cameraStatusButton").contains("span", "Off").should("be.visible");
     cy.get("#cameraStatusButton").should("not.have.class", "btn-success");
 
-    cy.get("#cameraButton")
-      .contains("span", "Off")
-      .should("be.visible");
+    cy.get("#cameraButton").contains("span", "Off").should("be.visible");
     cy.get("#cameraButton").should("not.have.class", "btn-success");
     cy.get("#videoFeed").should("be.visible");
     cy.get("#videoFeed")
       .should("have.attr", "src")
       .should("include", "xscreen.png");
-
-    cy.get("#fiveVoltRelayButton").click();
-
-    cy.contains("Relay 7 off").should("be.visible");
-    cy.get("#fiveVoltRelayButton")
-      .contains("span", "Off")
-      .should("be.visible");
-    cy.get("#fiveVoltRelayButton").should("not.have.class", "btn-success");
-
-    cy.get("#masterRelayStatusButton").click();
-
-    cy.contains("Switching Master Relay off.").should("be.visible");
-    cy.contains("Master Relay off").should("be.visible");
 
     cy.get("#masterRelayStatusButton")
       .contains("span", "Off", { timeout: 15000 })
