d5742635df51e008d9828fe4936717ebca318085
Christen Lofland <ChristenLofland@gmail.com>
2020-09-13 04:05:32 -0500

    Finish migrating old joystick code from Turtlebot to this package.

--- arlobot_ros/src/turtlebot_joy.cpp
+++ arlobot_ros/src/turtlebot_joy.cpp
@@ -26,7 +26,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * Coied to my ArloBot repository as the Turtlebot code is no longer maintained
+ * Copied to my ArloBot repository as the Turtlebot code is no longer maintained
  *
  */
 
