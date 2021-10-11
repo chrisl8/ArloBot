89dc855e38af50bb853bc46ef02376377210cd1d
Christen Lofland <ChristenLofland@gmail.com>
2021-10-11 15:05:00 -0500

    Stop collecting screenshots if they are not working.
    i.e. on a box without a display or X installed at all.

--- node/saveScreenShotForWeb.js
+++ node/saveScreenShotForWeb.js
@@ -5,6 +5,8 @@ const webModel = require('./webModel');
 const webModelFunctions = require('./webModelFunctions');
 const personalData = require('./personalData');
 
+let thisSystemHasAScreen = true;
+
 function resolvePath(str) {
   if (str.substr(0, 2) === '~/') {
     // eslint-disable-next-line no-param-reassign
@@ -18,7 +20,7 @@ function resolvePath(str) {
 }
 
 const saveScreenShotForWeb = () => {
-  if (!personalData.demoWebSite) {
+  if (thisSystemHasAScreen && !personalData.demoWebSite) {
     const oldFileName = resolvePath(
       `${personalData.web_folder}/xscreenOld.png`,
     );
@@ -29,22 +31,31 @@ const saveScreenShotForWeb = () => {
       console.log('Old screenshot does not exist yet!');
     }
     const command = `DISPLAY=:0.0 /usr/bin/import -silent -window root ${newFileName}`;
-    exec(command, () => {
-      // Argument options: error, stdout, stderr
-      const compare = `compare -metric RMSE ${newFileName} ${oldFileName} /dev/null`;
-      exec(compare, (error, stdout, stderr) => {
-        if (stderr) {
-          const output = stderr.split(' ')[0];
-          if (output[0]) {
-            if (output[0] > 0 && !webModel.cameraOn) {
-              webModelFunctions.update(
-                'videoSource',
-                `xscreen.png?_ts=${new Date().getTime()}`,
-              );
+    exec(command, (error, stdout, stderr) => {
+      if (stderr) {
+        console.error(
+          'This system does NOT appear to have a screen due to the following result:',
+        );
+        console.error(stderr);
+        console.error('Suspending screenshots.');
+        thisSystemHasAScreen = false;
+      } else {
+        // Argument options: error, stdout, stderr
+        const compare = `compare -metric RMSE ${newFileName} ${oldFileName} /dev/null`;
+        exec(compare, (error2, stdout2, stderr2) => {
+          if (stderr2) {
+            const output = stderr.split(' ')[0];
+            if (output[0]) {
+              if (output[0] > 0 && !webModel.cameraOn) {
+                webModelFunctions.update(
+                  'videoSource',
+                  `xscreen.png?_ts=${new Date().getTime()}`,
+                );
+              }
             }
           }
-        }
-      });
+        });
+      }
     });
   }
 };
