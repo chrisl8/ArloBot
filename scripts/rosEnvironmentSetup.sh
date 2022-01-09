9de39a890bad43ecae69b748b1114388cb2fed9b
Christen Lofland <ChristenLofland@gmail.com>
2022-01-08 18:26:11 -0600

    Replace hostname with IP in environment setup
    The IP is more reliable.
    
    On my home network the router often intercepts requests to the hostname and replies with an incorrect IP.
    Setting the IP instead seems reliable.

--- scripts/rosEnvironmentSetup.sh
+++ scripts/rosEnvironmentSetup.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
+# Grab and save the path to this script
+# http://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+# echo "${SCRIPTDIR}" # For debugging
 
 # Set up ROS Environment
-ROS_HOSTNAME=$(uname -n)
+ROS_IP=$(node "${SCRIPTDIR}/../node/ipAddress.js")
+export ROS_IP
+ROS_HOSTNAME=${ROS_IP}
 export ROS_HOSTNAME
 export ROS_MASTER_URI=http://localhost:11311
 export ROSLAUNCH_SSH_UNKNOWN=1
