4109e4719bece40be9f2358544e4b658215b16d9
Christen Lofland <Christen@Lofland.net>
2022-09-12 16:22:08 +0000

    Use the latest version of React.

--- website/src/index.js
+++ website/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './containers/App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
