f1e70da61778d6de7b6ac437249b3a575640df81
Christen Lofland <Christen@Lofland.net>
2023-03-25 01:03:39 +0000

    Fix eslint settings copied from wrong project.

--- node/.eslintrc.js
+++ node/.eslintrc.js
@@ -10,20 +10,17 @@
 // because it makes both AirBNB and Prettier happy and is automatic.
 // Less thinking is better than perfect and/or my preferences.
 
+// NOTE: This .eslintrc.cjs file is optimized for a Node.js app. I use slightly different
+// rules for Web front end code.
+
 module.exports = {
-  // https://gils-blog.tayar.org/posts/using-jsm-esm-in-nodejs-a-practical-guide-part-3/
-  parser: '@babel/eslint-parser', // Required to allow top level await
   parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
+    ecmaVersion: 2022,
   },
   extends: ['airbnb', 'prettier'],
   rules: {
-    // These are not code style or real errors, just "best practices" that really mean
-    // me making wonky code to fit requirements I don't need to fulfill.
-    // Someday I'll remove these as I get better. ;)
     'class-methods-use-this': 0,
-    'no-console': 'off', // As a node.js app, we clearly need console.
+    'no-console': 'off', // Sometimes we want to, okay?
     'no-prototype-builtins': 'off', // This seems like overkill
     'prefer-destructuring': 'off',
     'no-plusplus': 'off',
