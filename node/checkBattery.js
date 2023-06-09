d4522e3a57ab4c393cacf1328f0f11083f583459
Christen Lofland <christen@lofland.net>
2023-06-09 10:10:10 -0500

    Remove body-parser as it is now included with express. Use express built in version instead.

--- node/checkBattery.js
+++ node/checkBattery.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const glob = require('glob');
+const { glob } = require('glob');
 const personalData = require('./personalData');
 const webModel = require('./webModel');
 const webModelFunctions = require('./webModelFunctions');
