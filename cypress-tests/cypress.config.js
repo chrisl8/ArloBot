99e2d96364ef839e0d85c7805681b59bba089470
Christen Lofland <christen@lofland.net>
2023-09-15 17:09:50 -0500

    Update dependencies - Nothing interesting here.

--- cypress-tests/cypress.config.js
+++ cypress-tests/cypress.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: "http://localhost:3000/",
+    baseUrl: "http://localhost:5173/",
     testIsolation: false,
   },
 });
