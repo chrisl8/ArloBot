2eb875ec82ebc180530dc7bbe00a13396a7cd6f9
Christen Lofland <christen@lofland.net>
2024-10-07 12:49:37 -0500

    Move nvm init to run even if it was already installed before.

--- setup-jazzy.sh
+++ setup-jazzy.sh
@@ -474,9 +474,6 @@ if [ "$NVM_VERSION" != "$NVM_VERSION_LATEST" ]; then
   fi
 
   wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_TAG/install.sh" | bash
-  export NVM_DIR="${HOME}/.nvm"
-  # shellcheck source=/home/chrisl8/.nvm/nvm.sh
-  [[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
   export NVM_SYMLINK_CURRENT=true
   if ! (grep NVM_SYMLINK_CURRENT ~/.bashrc >/dev/null); then
@@ -490,6 +487,7 @@ if [ "$NVM_VERSION" != "$NVM_VERSION_LATEST" ]; then
 fi
 
 printf "\n${YELLOW}[Installing and Activating the Latest Node LTS version]${NC}\n"
+export NVM_DIR="${HOME}/.nvm"
 # shellcheck source=/home/chrisl8/.nvm/nvm.sh
 [[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install --lts
