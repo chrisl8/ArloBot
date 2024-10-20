6940a14a27c8e0a59d2290489eaab7d7bb98c265
Christen Lofland <christen@lofland.net>
2024-10-20 12:32:22 -0500

    Improve my rebuild script to work more reliably.

--- scripts/ros_rebuild.sh
+++ scripts/ros_rebuild.sh
@@ -20,6 +20,18 @@ fi
 # shellcheck source=/home/chrisl8/${ROS2_WS}/install/setup.bash
 source "${HOME}/${ROS2_WS}/install/setup.bash"
 
+if ! [[ -e ${HOME}/${ROS2_WS}/src/arlobot_ros ]]; then
+  ln -s "${HOME}/ArloBot/arlobot_ros" "${HOME}/${ROS2_WS}/src/"
+fi
+
+if ! [[ -e ${HOME}/${ROS2_WS}/src/arlobot_interfaces ]]; then
+  ln -s "${HOME}/ArloBot/arlobot_interfaces" "${HOME}/${ROS2_WS}/src/"
+fi
+
+if ! [[ -e ${HOME}/${ROS2_WS}/src/rplidar_ros ]]; then
+  ln -s "${HOME}/rplidar_ros" "${HOME}/${ROS2_WS}/src/rplidar_ros"
+fi
+
 cd "${HOME}/${ROS2_WS}"
 
 colcon build --symlink-install
